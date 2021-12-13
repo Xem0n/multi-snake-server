348c4de7b6b000403e6cf227945f61562b89ec56
Xem0n <biedukiewicz87@gmail.com>
2021-12-13 23:20:30 +0100

    add pingpong handling

--- client.go
+++ client.go
@@ -3,8 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
+)
+
+const (
+	readWait = time.Millisecond * 500
+	writeWait = time.Millisecond * 100
+	pingPeriod = time.Millisecond * 100
 )
 
 var upgrader = websocket.Upgrader{
@@ -19,9 +26,68 @@ var upgrader = websocket.Upgrader{
 type Client struct {
 	conn *websocket.Conn
 	game *Game
+	closed bool
+}
+
+func (client *Client) readPump() {
+	defer func() {
+		client.close()
+	}()
+
+	client.conn.SetReadDeadline(time.Now().Add(readWait))
+	client.conn.SetPongHandler(func(string) error { client.conn.SetReadDeadline(time.Now().Add(readWait)); return nil })
+
+	for {
+		_, _, err := client.conn.ReadMessage() 
+
+		if err != nil {
+			break
+		}
+
+		// todo: handle message
+	}
+}
+
+func (client *Client) writePump() {
+	pingTicker := time.NewTicker(pingPeriod)
+
+	defer func() {
+		pingTicker.Stop()
+		client.close()
+	}()
+
+	for {
+		select {
+		// todo: add and handle write channel 
+		case <-pingTicker.C:
+			client.conn.SetWriteDeadline(time.Now().Add(writeWait))
+
+			err := client.conn.WriteMessage(websocket.PingMessage, nil)
+
+			if err != nil {
+				return
+			}
+		}
+	}
+}
+
+func (client *Client) close() {
+	if client.closed {
+		return
+	}
+
+	client.conn.Close()
+	client.game.delete((client))
+	client.closed = true
+
+	log.Println("connection closed")
 }
 
 func serveWebsocket(game *Game, w http.ResponseWriter, r *http.Request) {
+	if (!game.canAddNewClient()) {
+		return
+	}
+
 	conn, err := upgrader.Upgrade(w, r, nil)
 
 	if err != nil {
@@ -31,8 +97,12 @@ func serveWebsocket(game *Game, w http.ResponseWriter, r *http.Request) {
 	client := &Client{
 		game: game,
 		conn: conn,
+		closed: false,
 	}
 
 	log.Printf("new connection: %s\n", conn.LocalAddr().String())
-	game.add(client)
+
+	go game.add(client)
+	go client.readPump()
+	go client.writePump()
 }
