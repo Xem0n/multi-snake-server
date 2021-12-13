d151df90ca3e102e59ce2280aa66fa933d121558
Xem0n <biedukiewicz87@gmail.com>
2021-12-13 23:19:19 +0100

    change clients slice to map and add basic game logic

--- game.go
+++ game.go
@@ -1,15 +1,38 @@
 package main
 
+import "log"
+
 type Game struct {
-	clients []*Client
+	clients map[*Client]bool
 }
 
 func newGame() *Game {
 	return &Game{
-		make([]*Client, 0),
+		make(map[*Client]bool),
 	}
 }
 
+func (game *Game) canStart() {
+	if len(game.clients) == 2 {
+		go game.start()
+	}
+}
+
+func (game *Game) start() {
+	log.Println("game started")
+	// todo: add game loop
+}
+
+func (game *Game) delete(client *Client) {
+	delete(game.clients, client)
+}
+
+func (game *Game) canAddNewClient() bool {
+	return len(game.clients) < 2
+}
+
 func (game *Game) add(client *Client) {
-	game.clients = append(game.clients, client)
+	game.clients[client] = true
+
+	game.canStart()
 }
