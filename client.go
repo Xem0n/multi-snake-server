a73211fb6995b509b149342811d362a555bdfa10
Xem0n <biedukiewicz87@gmail.com>
2021-12-13 21:57:01 +0100

    init websocket

--- client.go
+++ client.go
@@ -1,11 +1,39 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 type Client struct {
+	conn *websocket.Conn
 	game *Game
 }
 
 func serveWebsocket(game *Game, w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
 
+	if err != nil {
+		log.Println(err)
+	}
+
+	client := &Client{
+		game: game,
+		conn: conn,
+	}
+
+	game.add(client)
+
+	log.Printf("new connection: %s\n", conn.LocalAddr().String())
 }
