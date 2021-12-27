e78613d2252c9003f1c960fd5e524b77c540dfbe
Xem0n <biedukiewicz87@gmail.com>
2021-12-27 21:00:06 +0100

    add newSnake()

--- snake.go
+++ snake.go
@@ -1,9 +1,34 @@
 package main
 
+const STARTING_LENGTH = 50
+const VELOCITY = 1
+
 type Snake struct {
-	// todo: add parts
+	parts []*Part
+}
+
+func newSnake() *Snake {
+	// temporary starting pos
+	x := 10.
+	y := 500.
+	parts := make([]*Part, STARTING_LENGTH)
+
+	for i := 0; i < STARTING_LENGTH; i++ {
+		parts[i] = newPart(&Point{
+			x,
+			y,
+		})
+
+		y += VELOCITY
+	}
+
+	return &Snake{
+		parts,
+	}
 }
 
 func (snake *Snake) think(delta float64) {
-	// todo: move
+	// for _, part := range snake.parts {
+	// magic wth part
+	// }
 }
